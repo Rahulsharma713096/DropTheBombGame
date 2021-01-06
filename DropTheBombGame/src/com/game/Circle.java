e0c3535f6a421d443c873834342961673c7e0fc9
R S JALDS <R S JALDS@DESKTOP-94V18RE-RS>
2021-01-06 20:01:27 +0530

    6 Jan commit

---
+++ DropTheBombGame/src/com/game/Circle.java
@@ -0,0 +1,64 @@
+package com.game;
+
+public class Circle {
+	// TODO: add fields
+	double cRadius;
+	Point Origin;
+	/**
+	 * Constructor.
+	 * 
+	 * @param center  the center Point of the circle
+	 * @param radius  the radius of the circle
+	 */
+	//Declare fields
+	public Circle(Point center, double radius) {
+		cRadius=radius;
+		Origin=center;
+	}
+	
+	/**
+	 * Get the current center point of the circle.
+	 * 
+	 * @return the center point of the circle
+	 */
+	//return center
+	public Point getCenter() {
+		return Origin;
+		//throw new UnsupportedOperationException("TODO - implement");
+
+	}
+	
+	/**
+	 * Set the center point of the circle.
+	 * 
+	 * @param center the (new) center point of the circle
+	 */
+	//set center locally
+	public void setCenter(Point center) {
+		//throw new UnsupportedOperationException("TODO - implement");
+		Origin = center;
+	}
+
+	/**
+	 * Get the current radius.
+	 * 
+	 * @return the current radius
+	 */
+	//return radius
+	public double getRadius() {
+		return cRadius;
+		//throw new UnsupportedOperationException("TODO - implement");
+	}
+
+	/**
+	 * Set the radius.
+	 * 
+	 * @param radius the (new) radius of the circle
+	 */
+	//Set radius locally
+	public void setRadius(double radius) {
+		//throw new UnsupportedOperationException("TODO - implement");
+		cRadius = radius;
+	}
+
+}
